8022a58fc61ae27d72b2ddb48cb5b6df79667f97
merima98 <merimaceranic98@gmail.com>
2022-04-27 13:35:22 +0200

    Added example project for practicing supabase.

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./index.css";
+import { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
+import Auth from "./Auth";
+import Account from "./Account";
+
+function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    console.log("I am in use effesct");
+    setSession(supabase.auth.session());
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className="container" style={{ padding: "2rem" }}>
+        {!session ? (
+          <Auth />
+        ) : (
+          <Account key={session.user.id} session={session} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
